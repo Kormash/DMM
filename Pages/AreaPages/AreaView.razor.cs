63ef324d32ad68b6be7065ff5432e24830527c81
Erik Häregård <erik.haregard@iths.se>
2021-06-08 14:05:52 +0200

    Implemented and improved DiceRoller

--- Pages/AreaPages/AreaView.razor.cs
+++ Pages/AreaPages/AreaView.razor.cs
@@ -23,19 +23,49 @@ namespace DMM.Pages.AreaPages
         HttpClient Http { get; set; }
         //Variables
         string Output = "Temp Text";
+        int diceType = 20;
+        int diceNumber = 1;
+
+        List<Dice> d100List = new();
+        List<Dice> d20List = new();
+        List<Dice> d12List = new();
+        List<Dice> d10List = new();
+        List<Dice> d8List = new();
+        List<Dice> d6List = new();
+        List<Dice> d4List = new();
+
+        DiceModel diceModel = new();
+
         [Parameter]
         public int AreaId { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            await DiceAPICall();
+            
         }
 
-        public async Task DiceAPICall()
+        public async Task RollDice()
+        {
+            await DiceAPICall(diceNumber, diceType);
+            await UpdateModel();
+        }
+
+        public async Task ClearDices()
+        {
+            d100List = new();
+            d20List = new();
+            d12List = new();
+            d10List = new();
+            d8List = new();
+            d6List = new();
+            d4List = new();
+        }
+
+        public async Task DiceAPICall(int diceNumber, int diceType)
         {
             try
             {
-                HttpResponseMessage response = await Http.GetAsync("https://localhost:44327/Dice/2/5");
+                HttpResponseMessage response = await Http.GetAsync($"https://localhost:44327/Dice/{diceNumber}/{diceType}");
                 response.EnsureSuccessStatusCode();
                 Output = await response.Content.ReadAsStringAsync();
                 // Above three lines can be replaced with new helper method below
@@ -47,5 +77,79 @@ namespace DMM.Pages.AreaPages
                 Console.WriteLine("Message :{0} ", e.Message);
             }
         }
+
+        public async Task UpdateModel()
+        {
+            String[] DiceStrings = Output.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            string diceTypeString = Regex.Replace(DiceStrings[0], "\\[\\{\"diceType\":", "");
+            diceModel.DiceType = int.Parse(diceTypeString);
+
+            string diceNumberString = Regex.Replace(DiceStrings[1], "\"diceNumber\":", "");
+            diceModel.DiceNumber = int.Parse(diceNumberString);
+
+            string diceSumString = Regex.Replace(DiceStrings[2], "\"diceSum\":", "");
+            diceModel.DiceSum = int.Parse(diceSumString);
+
+            string resultString = Regex.Replace(DiceStrings[3], "\"", "");
+            resultString = Regex.Replace(resultString, "resultString:", "");
+            resultString = Regex.Replace(resultString, "\\}\\]", "");
+            diceModel.ResultString = resultString;
+
+            String[] DiceResults = resultString.Split(" + ", StringSplitOptions.RemoveEmptyEntries);
+            foreach(var result in DiceResults)
+            {
+                Dice d = new();
+                d.DiceType = diceModel.DiceType;
+                d.DiceResult = int.Parse(result);
+
+                if(d.DiceType == 100)
+                {
+                    d100List.Add(d);
+                }
+                else if(d.DiceType == 20)
+                {
+                    d20List.Add(d);
+                }
+                else if (d.DiceType == 12)
+                {
+                    d12List.Add(d);
+                }
+                else if (d.DiceType == 10)
+                {
+                    d10List.Add(d);
+                }
+                else if (d.DiceType == 8)
+                {
+                    d8List.Add(d);
+                }
+                else if (d.DiceType == 6)
+                {
+                    d6List.Add(d);
+                }
+                else if (d.DiceType == 4)
+                {
+                    d4List.Add(d);
+                }
+            }
+
+        }
+    }
+
+    public class DiceModel
+    {
+        public int DiceType { get; set; }
+
+        public int DiceNumber { get; set; }
+
+        public int DiceSum { get; set; }
+
+        public string ResultString { get; set; }
+    }
+
+    public class Dice 
+    {
+        public int DiceType { get; set; }
+        public int DiceResult { get; set; }
     }
 }
