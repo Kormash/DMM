56a01f322024287ae1530d594ef72894dd484e2f
Expell <57598916+Theseffix@users.noreply.github.com>
2021-06-15 10:21:56 +0200

    CoolstufBroski

---
+++ Pages/AdminPages/PrivacyPolicy.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DMM.Services;
+using Blazority;
+using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Components.Authorization;
+using DMM.Models.Entities;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Identity;
+using BlazorInputFile;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DMM.Pages.AdminPages
+{
+    public partial class PrivacyPolicy : ComponentBase
+    {
+
+        protected override async Task OnInitializedAsync()
+        {
+            
+        }
+    }
+}
